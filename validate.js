2d21dab15ba895feb608c287f9c1055fd30bbec8
Arsalaann <arsalankhanxk@gmail.com>
2023-06-23 13:49:55 +0530

    Email validate

--- validate.js
+++ validate.js
@@ -2,6 +2,10 @@ function validateForm(){
 	const form=document.getElementById('my-form');
 	if(form.elements[0].value[0]>'Z' || form.elements[0].value.length<3){
 		document.querySelectorAll("small")[0].innerHTML="name is incorrect";
+		return false;
 	}
-	return false;
+	if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.elements[2].value)==false){
+		document.querySelectorAll("small")[2].innerHTML="Invalid Email";
+		return false;
+	}
 }
